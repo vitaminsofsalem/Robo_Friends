8b4a3e7383c3cbd4c936badbf5eceda2cec47e2b
vitaminsofsalem <jokydokyduck@gmail.com>
2022-04-25 23:45:40 +0200

    Basic Card Implementation

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "tachyons";
+
+export default function Card({ id, name, email }) {
+  return (
+    <div className="tc bg-yellow dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+      <div>
+        <h1>{name}</h1>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+}
