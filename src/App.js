c32a5723b1fb114553d55f255f7a9274ab8e00ea
vitaminsofsalem <jokydokyduck@gmail.com>
2022-04-26 00:13:24 +0200

    Implemented Card List Wrapper Comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import Card from "./components/Card";
+import CardList from "./components/CardList";
 import { robots } from "./database/robots";
 
 const App = () => {
   return (
     <>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      <CardList robots={robots} />
     </>
   );
 };
